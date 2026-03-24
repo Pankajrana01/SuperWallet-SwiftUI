43b35acbfdcb9807f55538ce67945628e09519bd
Pankaj Rana <pankajrana.rana01@gmail.com>
2026-03-24 10:38:42 +0530

    Setup SuperWallet SwiftUI project with MVVM architecture

---
+++ SuperWalletAppTests/SuperWalletAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SuperWalletAppTests.swift
+//  SuperWalletAppTests
+//
+//  Created by Pankaj Rana on 13/08/25.
+//
+
+import Testing
+@testable import SuperWalletApp
+
+struct SuperWalletAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
